991427cc4d8f9c642caa443f3d94a0d9dd444f7d
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-01-10 21:01:03 +0200

    Add method for add element in list

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,60 @@
 ﻿#include <iostream>
 
+template<typename T>
+struct Node {
+	T value;
+	Node* next;
+	Node* prev;
+};
+
+template<typename T>
+class List {
+private:
+	Node<T>* head;
+	unsigned long size;
+public:
+	explicit List(){
+		this->head = NULL;
+		this->size = 0;
+	}
+	void addToList(T value) {
+		Node<T>* tmp = this->head;
+		if (tmp == NULL) {
+			tmp = new Node<T>;
+			tmp->value = value;
+			tmp->next = nullptr;
+			tmp->prev = nullptr;
+			this->head = tmp;
+		}
+		else {
+			while (tmp->next != nullptr) {
+				tmp = tmp->next;
+			}
+			tmp->next = new Node<T>;
+			tmp->next->value = value;
+			tmp->next->next = nullptr;
+			tmp->next->prev = tmp;
+		}
+		this->size++;
+	}
+
+	void showList() {
+		Node<T>* tmp = this->head;
+		while (tmp != nullptr) {
+			std::cout << tmp->prev << "\t" << tmp << "\t" << tmp->value << "\t" << tmp->next << "\n";
+			tmp = tmp->next;
+		}
+		delete tmp;
+	}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	List<int> num;
+	num.addToList(1);
+	num.addToList(2);
+	num.addToList(3);
+	num.addToList(4);
+	num.addToList(5);
+	num.showList();
 }
