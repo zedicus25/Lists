734697b274f2324b6f59874bc450d637e63f7434
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-01-25 20:44:21 +0200

    Add some changes

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,30 @@
 ﻿#include <iostream>
-
+#include <exception>
+#include <string>
 template<typename T>
 struct Node {
 	T value;
 	Node* next;
 	Node* prev;
+	
 };
 
+class ListException : public std::exception
+{
+private:
+	std::string error;
+	int code;
+public:
+	ListException(int code) : code(code) {
+		if (code == 1) {
+			this->error = "Out of bounds list";
+		}
+	}
+
+	const char* what() const noexcept override {
+		return this->error.c_str();
+	}
+};
 template<typename T>
 class List {
 private:
@@ -16,6 +34,10 @@ public:
 	explicit List(){
 		this->head = NULL;
 		this->size = 0;
+	}
+
+	unsigned long getSize() {
+		return this->size;
 	}
 
 	void addToList(T value) {
@@ -38,7 +60,71 @@ public:
 		}
 		this->size++;
 	}
-	void removeFromList(T value) {
+
+	void editElement(int id, T value) {
+		if (id < 0 || id >this->size) {
+			throw ListException(1);
+		}
+		Node<T>* tmp = this->head;
+		for (int i = 0; i < id; i++)
+		{
+			if (tmp->next != nullptr) {
+				tmp = tmp->next;
+			}
+		}
+		tmp->value = value;
+	}
+
+	T getElement(int id) {
+		if (id < 0 || id >this->size) {
+			throw ListException(1);
+		}
+		Node<T>* tmp = this->head;
+		for (int i = 0; i < id; i++)
+		{
+			if (tmp->next != nullptr) {
+				tmp = tmp->next;
+			}
+		}
+		return tmp->value;
+	}
+
+	void removeFromList(int index) {
+		if (index < 0 || index > this->size) {
+			throw ListException(1);
+		}
+		Node<T>* forDelete = NULL;
+		if (index == 0) {
+			forDelete = this->head;
+			this->head = this->head->next;
+			this->head->prev = NULL;
+		}
+		else {
+			Node<T>* tmp = this->head;
+			forDelete = tmp;
+			int i = 1;
+			while (tmp->next != nullptr) {
+				if (i == index && tmp->next != nullptr) {
+					forDelete = tmp->next;
+					break;
+				}
+				i++;
+				tmp = tmp->next;
+			}
+			if (tmp->next->next == NULL) {
+				tmp->next = NULL;
+				tmp->prev = tmp;
+			}
+			else {
+				tmp->next = tmp->next->next;
+				tmp->next->prev = tmp;
+			}
+		}
+		delete forDelete;
+		this->size--;
+	}
+
+	/*void removeFromList(T value) {
 		if (this->isInList(value)) {
 			Node<T>* forDelete = NULL;
 			if (this->head->value == value) {
@@ -69,16 +155,6 @@ public:
 			this->size--;
 		}
 	}
-
-	void showList() {
-		Node<T>* tmp = this->head;
-		while (tmp != nullptr) {
-			std::cout << tmp->prev << "\t" << tmp << "\t" << tmp->value << "\t" << tmp->next << "\n";
-			tmp = tmp->next;
-		}
-		delete tmp;
-	}
-
 	bool isInList(T value) {
 		bool isIn = false;
 		Node<T>* tmp = this->head;
@@ -92,20 +168,74 @@ public:
 		tmp = nullptr;
 
 		return isIn;
+	}*/
+
+	void showList() {
+		Node<T>* tmp = this->head;
+		while (tmp != nullptr) {
+			std::cout << tmp->value << "\n";
+			tmp = tmp->next;
+		}
+		delete tmp;
+	}
+
+	T operator[](const int index) {
+		if (index < 0 || index >this->size) {
+			throw ListException(1);
+		}
+		return this->getElement(index);
+	}
+
+	List<T>* operator+(List<T> other) {
+		Node<T>* tmp = this->head;
+		while (tmp->next != nullptr)
+		{
+			tmp = tmp->next;
+		}
+		Node<T>* tmp1 = other.head;
+		while (tmp1->next != nullptr)
+		{
+			tmp->next = tmp1;
+			tmp1 = tmp1->next;
+		}
 	}
 };
 
 int main()
 {
 	List<int> num;
-	num.addToList(1);
-	num.addToList(2);
+	num.addToList(5);
 	num.addToList(3);
 	num.addToList(4);
-	num.addToList(5);
-	num.showList();
-	std::cout << "\n";
-	num.removeFromList(1);
-	num.showList();
+
+
+	/*try
+	{
+		num.removeFromList(2);
+	}
+	catch (ListException error)
+	{
+		std::cout << error.what() << "\n";
+	}*/
+	/*try
+	{
+		num.editElement(15, 15);
+	}
+	catch (ListException error)
+	{
+		std::cout << error.what() << "\n";
+	}*/
+
+	/*try
+	{
+		std::cout <<"Get element " << num[4] << "\n";
+	}
+	catch (ListException error)
+	{
+		std::cout <<  error.what() << "\n";
+	}*/
+	
+	//num.showList();
+
 
 }
